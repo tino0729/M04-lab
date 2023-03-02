4c7771c0c2f1851aad469a4bd26a5b99261f0c3d
Clemente De Rosa <100239573+tino0729@users.noreply.github.com>
2023-03-01 19:13:37 -0600

    Add files via upload

---
+++ mo4 lab.py
@@ -0,0 +1,13 @@
+
+import sqlalchemy as sa
+
+engine = sa.create_engine('sqlite:///books.db')
+conn = engine.connect()
+sql = sa.text('SELECT title from book')
+rows = conn.execute(sql)
+
+for row in rows:
+    print('title:', row[0])
+
+conn.close()
+engine.dispose()
